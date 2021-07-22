f7c1b3ca02b6ff436a74ac94bc83956ebd3038b3
mo7ammed-24 <81976930+mo7ammed-24@users.noreply.github.com>
2021-07-22 16:17:36 +0300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,4 @@ fun main() {
     println(centuryFromYear(1997))
 }
 
-fun centuryFromYear(year: Int): Int {
-
-    var test =(year-1)/100
-    test =test+1
-    return test}
+fun centuryFromYear(year: Int)=((year-1)/100)+1
